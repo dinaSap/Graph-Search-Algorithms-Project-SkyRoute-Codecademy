6022d7d172ad3874ecb8cf17817fc1e242795a25
dinaSap <49748840+dinaSap@users.noreply.github.com>
2020-04-29 02:07:21 +0400

    Search Functions

--- graph.py
+++ graph.py
@@ -21,6 +21,7 @@ def dfs(graph, current_vertex, target_value, visited = None):
   if visited is None:
     visited = []
 	
+	
   visited.append(current_vertex)
   
   if current_vertex == target_value:
